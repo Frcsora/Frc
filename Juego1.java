ced3b48639197d265bd083924f64a3dc15c3446a
Frcsora <150892201+Frcsora@users.noreply.github.com>
2023-11-26 12:42:17 +0100

    Add files via upload

--- Juego1.java
+++ Juego1.java
@@ -32,23 +32,96 @@ public class Juego1 {
         }while(on);
     }  
    
-    static void salida(){
-        do{
-                a = sc.nextLine().toUpperCase();
-                if((!"S".equals(a) && !"N".equals(a))){
-                    System.out.println("Quieres jugar otra partida? S/N: ");
-                }
-            }while(!"S".equals(a) && !"N".equals(a));
-            if("N".equals(a)){
-                on=false;
-                marcador();
-            } 
+    static boolean validacion(Object resposta){     
+        
+        return resposta instanceof Integer;
     }
+    
+    static int random(int x){
+        return (int)(Math.random() * x + 1);
+    }   
     static void entrada(){
         System.out.print("Jugador 1 escribe tu nombre: ");
         nombre1=sc.nextLine();
         System.out.print("Jugador 2 escribe tu nombre: " );
         nombre2=sc.nextLine();
+    }
+    static void marcador(){
+        System.out.println("Marcador:");
+        System.out.println(nombre1+": "+marcador1);
+        System.out.println(nombre2+": "+marcador2);
+    }
+
+    static void juego(){
+        int turnos = random(20);
+        while(turnos > 0){
+
+            if(jugador1){
+                System.out.println("Turno de "+nombre1);
+            }
+            else{
+                System.out.println("Turno de "+nombre2);
+            }
+            /*Creo otro número secreto esta vez dentro del bucle, ya
+            que en un principio lo cree fuera y luego eso dio problemas
+            porque no canmbiaba aunque se acertase*/
+            
+            numSecret = random(100);
+               
+            /*Aquí es donde se va a llevar a cabo el juego, solo se sale
+            acertando o cuando explota*/
+            System.out.println("Adivina el numero entre 1 i 100");              
+                
+            /*Con el siguiente do... while nos aseguramos de que el numero
+            introducido es un int*/
+            do {
+                System.out.println("Introduce un numero:");
+                while (!sc.hasNextInt()) {
+                    System.out.println("Por favor, introduce un numero valido.");
+                    sc.next();  
+                }
+                resposta = sc.nextInt();
+            } while (!validacion(resposta));
+                               
+            while(turnos > 0 && resposta != numSecret){
+                if(resposta < numSecret){
+                    System.out.println("MAS!!");
+                }
+                else{
+                    System.out.println("MENOS!!");
+                }
+                    
+                do {
+                    System.out.println("Introduce un numero:");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("Por favor, introduce un numero valido.");
+                        sc.next();
+                    }
+                    resposta = sc.nextInt();
+                } while (!validacion(resposta));
+                turnos--;
+                    
+                if(resposta == numSecret){
+                    System.out.println("CORRECTO!!");
+                }
+            }
+                
+            if(resposta!=numSecret){
+                System.out.println("El numero era " + numSecret);
+            }
+               
+            /*Si la respuesta se ha acertado se cambia el turno del jugador*/
+            if(jugador1){
+                if(numSecret == resposta){
+                    jugador1=false;
+                }
+            }
+            else{
+                if(numSecret == resposta){
+                    jugador1=true;
+                }
+            }
+        }        
     }
     static void ganador(){
         if(jugador1)
@@ -63,90 +136,16 @@ public class Juego1 {
                 marcador1++;
             }
     }
-    public static boolean validacion(Object resposta){     
-        
-        return resposta instanceof Integer;
-    }
-    public static void marcador(){
-        System.out.println("Marcador:");
-        System.out.println(nombre1+": "+marcador1);
-        System.out.println(nombre2+": "+marcador2);
-    }
-    public static int random(int x){
-        return (int)(Math.random() * x + 1);
-    }   
-
-    static void juego(){
-        int turnos = random(20);
-        while(turnos > 0){
-
-                if(jugador1){
-                    System.out.println("Turno de "+nombre1);
+    static void salida(){
+        do{
+                a = sc.nextLine().toUpperCase();
+                if((!"S".equals(a) && !"N".equals(a))){
+                    System.out.println("Quieres jugar otra partida? S/N: ");
                 }
-                else{
-                    System.out.println("Turno de "+nombre2);
-                }
-                /*Creo otro número secreto esta vez dentro del bucle, ya
-                que en un principio lo cree fuera y luego eso dio problemas
-                porque no canmbiaba aunque se acertase*/
-                
-                numSecret = random(100);
-                
-                /*Aquí es donde se va a llevar a cabo el juego, solo se sale
-                acertando o cuando explota*/
-                System.out.println("Adivina el numero entre 1 i 100");              
-                
-                /*Con el siguiente do... while nos aseguramos de que el numero
-                introducido es un int*/
-                do {
-                    System.out.println("Introduce un numero:");
-                    while (!sc.hasNextInt()) {
-                        System.out.println("Por favor, introduce un numero valido.");
-                        sc.next();  
-                    }
-                    resposta = sc.nextInt();
-                } while (!validacion(resposta));
-                                
-                /*System.out.println(numSecret);*/
-                while(turnos > 0 && resposta != numSecret){
-                    if(resposta < numSecret){
-                        System.out.println("MAS!!");
-                    }
-                    else{
-                        System.out.println("MENOS!!");
-                    }
-                    
-                    do {
-                        System.out.println("Introduce un numero:");
-                        while (!sc.hasNextInt()) {
-                            System.out.println("Por favor, introduce un numero valido.");
-                            sc.next();
-                        }
-                        resposta = sc.nextInt();
-                    } while (!validacion(resposta));
-                    turnos--;
-                    
-                    if(resposta == numSecret){
-                        System.out.println("CORRECTO!!");
-                    }
-                }
-                
-                if(resposta!=numSecret){
-                    System.out.println("El numero era " + numSecret);
-                }
-                
-                /*Si la respuesta se ha acertado se cambia el turno del jugador*/
-                if(jugador1){
-                    if(numSecret == resposta){
-                        jugador1=false;
-                    }
-                }
-                else{
-                    if(numSecret == resposta){
-                        jugador1=true;
-                    }
-                }
-
-            }
+            }while(!"S".equals(a) && !"N".equals(a));
+            if("N".equals(a)){
+                on=false;
+                marcador();
+            } 
     }
 }
